0ddbc54f9975c8d519f8239d122ae9b5478462b2
Adam Piątyszek <ediap@users.sourceforge.net>
2006-05-11 12:45:21 +0000

    Merged rev. 463:464 from the main trunk (see ChangeLog for more info)
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/branches/itpp-3-10@465 a6533b66-300f-0410-b061-b36de6c4fb2d

--- tests/window_test.cpp
+++ tests/window_test.cpp
@@ -1,4 +1,4 @@
-#include <itpp/itbase.h>
+#include <iomanip>
 
 using std::cout;
 using std::endl;
@@ -6,26 +6,27 @@ using namespace itpp;
 
 int main(void)
 {
+  // This is a hack for improper rounding under MinGW
+  cout.precision(8);
 
- cout << "================================" << endl;
- cout << "    Test of window functions " << endl;
- cout << "================================" << endl;
+  cout << "================================" << endl;
+  cout << "    Test of window functions    " << endl;
+  cout << "================================" << endl;
 
- cout << "hamming(32) = " << round_to_zero(hamming(32)) << endl;
- cout << "hamming(128) = " << round_to_zero(hamming(128)) << endl;
+  cout << "hamming(32) = " << round_to_zero(hamming(32)) << endl;
+  cout << "hamming(128) = " << round_to_zero(hamming(128)) << endl;
 
- cout << "hanning(32) = " << round_to_zero(hanning(32)) << endl;
- cout << "hanning(128) = " << round_to_zero(hanning(128)) << endl;
+  cout << "hanning(32) = " << round_to_zero(hanning(32)) << endl;
+  cout << "hanning(128) = " << round_to_zero(hanning(128)) << endl;
 
- cout << "hann(32) = " << round_to_zero(hann(32)) << endl;
- cout << "hann(128) = " << round_to_zero(hann(128)) << endl;
+  cout << "hann(32) = " << round_to_zero(hann(32)) << endl;
+  cout << "hann(128) = " << round_to_zero(hann(128)) << endl;
 
- cout << "blackman(32) = " << round_to_zero(blackman(32)) << endl;
- cout << "blackman(128) = " << round_to_zero(blackman(128)) << endl;
+  cout << "blackman(32) = " << round_to_zero(blackman(32)) << endl;
+  cout << "blackman(128) = " << round_to_zero(blackman(128)) << endl;
 
- cout << "triang(32) = " << round_to_zero(triang(32)) << endl;
- cout << "triang(128) = " << round_to_zero(triang(128)) << endl;
+  cout << "triang(32) = " << round_to_zero(triang(32)) << endl;
+  cout << "triang(128) = " << round_to_zero(triang(128)) << endl;
 
- return 0;
-
+  return 0;
 }
