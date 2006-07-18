fba6544f2f7355054a42efd5f72f90d6838aa478
Adam Piątyszek <ediap@users.sourceforge.net>
2006-07-18 07:53:57 +0000

    Merged rev. 544:545 from the main trunk (see ChangeLog for more info).
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/branches/itpp-3-10@546 a6533b66-300f-0410-b061-b36de6c4fb2d

--- itpp/sources.mk
+++ itpp/sources.mk
@@ -1,5 +1,4 @@
 h_sources = \
-	$(top_builddir)/itpp/config.h \
 	$(top_srcdir)/itpp/itbase.h \
 	$(top_srcdir)/itpp/itcomm.h \
 	$(top_srcdir)/itpp/itconfig.h \
@@ -7,5 +6,3 @@ h_sources = \
 	$(top_srcdir)/itpp/itmex.h \
 	$(top_srcdir)/itpp/itprotocol.h \
 	$(top_srcdir)/itpp/itsrccode.h
-
-cpp_sources =
