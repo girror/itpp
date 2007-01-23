f1164887cdb43af457ac5a043e4dadf32f11e4a4
Adam Piątyszek <ediap@users.sourceforge.net>
2007-01-23 22:25:38 +0000

    Added missing architecture checks, which should fix bugs [1641691] and [1640347]. BTW, fixed interchanged little- and big-endian definitions (e.g. i386 and x86_64 are little-endian, whereas SPARC is big-endian). 64-bit special typedefs commented.
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/branches/itpp-3-10@872 a6533b66-300f-0410-b061-b36de6c4fb2d

--- itpp/base/machdep.h
+++ itpp/base/machdep.h
@@ -1,7 +1,7 @@
 /*!
  * \file
  * \brief Machine dependent formats for binary storage
- * \author Tobias Ringstrom
+ * \author Tobias Ringstrom and Adam Piatyszek
  * 
  * $Date$
  * $Revision$
@@ -11,7 +11,7 @@
  * IT++ - C++ library of mathematical, signal processing, speech processing,
  *        and communications classes and functions
  *
- * Copyright (C) 1995-2006  (see AUTHORS file for a list of contributors)
+ * Copyright (C) 1995-2007  (see AUTHORS file for a list of contributors)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -79,20 +79,42 @@ namespace itpp {
   inline void  little_endian(it_s8 x, it_s8 &y) { y = x; }
   inline void  little_endian(it_u8 x, it_u8 &y) { y = x; }
 
-  //Determine the endianity (Little or Big):
-#if defined(sparc)
-#define __LITTLE_ENDIAN__
-#elif defined(i386) || defined(_M_IX86) || defined(__x86_64__) || defined(alpha) || defined(vms)
-#define __BIG_ENDIAN__
+  /*
+   * Big endian: _AIX, AIX, sparc, __sparc__, __mc68000__, __m68k__, MIPSEB,
+   * __MIPSEB__, __ARMEB, HPPA, __hppa__, PPC, __ppc_, __PPC__, __powerpc__.
+   * 
+   * Little endian: i386, __i386__, __x86_64__, __amd64__, __sh__, __vax__
+   * (yes, really!), MIPSEL, __MIPSEL__, and __ARMEL__, __alpha, __alpha__,
+   * __ia64__.
+   *
+   * It seems that ppc, hppa, ia64, alpha, sparc, mips, arm and superh have
+   * both big and little endian modes.
+   */
+#if defined(_AIX) || defined(AIX)					\
+  || defined(sparc) || defined(__sparc__)				\
+  || defined(__mc68000__) || defined(__mk68k__)				\
+  || defined(MIPSEB) || defined(__MIPSEB__)				\
+  || defined(__ARMEB)							\
+  || defined(HPPA) || defined(__hppa__)					\
+  || defined(PPC) || defined(__ppc__) || defined(__PPC__)		\
+  || defined(__powerpc__)						\
+  || defined(__s390__) || defined(__s390x__)
+#  define __BIG_ENDIAN__
+#elif defined(i386) || defined(__i386__)       \
+  || defined(__x86_64__) || defined(__amd64__) \
+  || defined(MIPSEL) || defined(__MIPSEL__)    \
+  || defined(__alpha) || defined(__alpha__)
+#  define __LITTLE_ENDIAN__
 #endif
 
-#if defined(__LITTLE_ENDIAN__)
+
+#if defined(__BIG_ENDIAN__)
 
   //------------------------------------------------------
-  // Little Endian
+  // Big Endian
   //------------------------------------------------------
 
-#define IT_ENDIANITY 1234
+#define IT_ENDIANITY 4321
 
   inline it_s16 big_endian(it_s16 x) { return x; }
   inline it_u16 big_endian(it_u16 x) { return x; }
@@ -123,26 +145,26 @@ namespace itpp {
   inline void little_endian(it_f64 x, it_f64 &y) { REV_8(&x,&y); }
 
   //Additions for some 64 bit architechtures
-#if defined(sparc)
-  typedef signed long        it_s64;
-  typedef unsigned long      it_u64;
-  inline it_s64 big_endian(it_s64 x) { return x; }
-  inline it_u64 big_endian(it_u64 x) { return x; }
-  inline it_s64 little_endian(it_s64 x) { it_s64 y; REV_4(&x,&y); return y; }
-  inline it_u64 little_endian(it_u64 x) { it_u64 y; REV_4(&x,&y); return y; }
-  inline void big_endian(it_s64 x, it_s64 &y) { y = x; }
-  inline void big_endian(it_u64 x, it_u64 &y) { y = x; }
-  inline void little_endian(it_s64 x, it_s64 &y) { REV_4(&x,&y); }
-  inline void little_endian(it_u64 x, it_u64 &y) { REV_4(&x,&y); }
-#endif
+// #if defined(sparc)
+//   typedef signed long        it_s64;
+//   typedef unsigned long      it_u64;
+//   inline it_s64 big_endian(it_s64 x) { return x; }
+//   inline it_u64 big_endian(it_u64 x) { return x; }
+//   inline it_s64 little_endian(it_s64 x) { it_s64 y; REV_4(&x,&y); return y; }
+//   inline it_u64 little_endian(it_u64 x) { it_u64 y; REV_4(&x,&y); return y; }
+//   inline void big_endian(it_s64 x, it_s64 &y) { y = x; }
+//   inline void big_endian(it_u64 x, it_u64 &y) { y = x; }
+//   inline void little_endian(it_s64 x, it_s64 &y) { REV_4(&x,&y); }
+//   inline void little_endian(it_u64 x, it_u64 &y) { REV_4(&x,&y); }
+// #endif
 
-#elif defined(__BIG_ENDIAN__)
+#elif defined(__LITTLE_ENDIAN__)
 
   //------------------------------------------------------
-  // Big Endian
+  // Little Endian
   //------------------------------------------------------
 
-#define IT_ENDIANITY 4321
+#define IT_ENDIANITY 1234
 
   //32 bit architechures (default)
   inline it_s16 big_endian(it_s16 x) { it_s16 y; REV_2(&x,&y); return y; }
@@ -174,18 +196,18 @@ namespace itpp {
   inline void little_endian(it_f64 x, it_f64 &y) { y = x; }
 
   //Additions for some 64 bit architechtures
-#if defined(alpha) || defined(__x86_64__)
-  typedef signed long        it_s64;
-  typedef unsigned long      it_u64;
-  inline it_s64 big_endian(it_s64 x) { it_s64 y; REV_4(&x,&y); return y; }
-  inline it_u64 big_endian(it_u64 x) { it_u64 y; REV_4(&x,&y); return y; }
-  inline it_s64 little_endian(it_s64 x) { return x; }
-  inline it_u64 little_endian(it_u64 x) { return x; }
-  inline void big_endian(it_s64 x, it_s64 &y) { REV_4(&x,&y); }
-  inline void big_endian(it_u64 x, it_u64 &y) { REV_4(&x,&y); }
-  inline void little_endian(it_s64 x, it_s64 &y) { y = x; }
-  inline void little_endian(it_u64 x, it_u64 &y) { y = x; }
-#endif
+// #if defined(alpha) || defined(__x86_64__) || defined(__s390x__)
+//   typedef signed long        it_s64;
+//   typedef unsigned long      it_u64;
+//   inline it_s64 big_endian(it_s64 x) { it_s64 y; REV_4(&x,&y); return y; }
+//   inline it_u64 big_endian(it_u64 x) { it_u64 y; REV_4(&x,&y); return y; }
+//   inline it_s64 little_endian(it_s64 x) { return x; }
+//   inline it_u64 little_endian(it_u64 x) { return x; }
+//   inline void big_endian(it_s64 x, it_s64 &y) { REV_4(&x,&y); }
+//   inline void big_endian(it_u64 x, it_u64 &y) { REV_4(&x,&y); }
+//   inline void little_endian(it_s64 x, it_s64 &y) { y = x; }
+//   inline void little_endian(it_u64 x, it_u64 &y) { y = x; }
+// #endif
 
 #else
 
