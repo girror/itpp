8cce80eac06a22b356bf18859398071bda2e184e
Adam Piątyszek <ediap@users.sourceforge.net>
2007-05-15 19:42:31 +0000

    Fixed linking errors when trying to use Hadamard and Walsh-Hadamard transform functions. This fixes bug report [1719146] submitted by Stephan Ludwig.
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/branches/itpp-3-10@1026 a6533b66-300f-0410-b061-b36de6c4fb2d

--- itpp/base/transforms.h
+++ itpp/base/transforms.h
@@ -29,9 +29,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * -------------------------------------------------------------------------
- * 
- * This file uses the FFTW package version 3.0.x that is distributed under 
- * the GNU GPL licence. For details see http://www.fftw.org/.
  */
 
 #ifndef TRANSFORMS_H
@@ -39,6 +36,7 @@
 
 #include <itpp/base/vec.h>
 #include <itpp/base/mat.h>
+#include <itpp/base/matfunc.h>
 
 
 namespace itpp {
@@ -61,26 +59,26 @@ namespace itpp {
     \code Y = fft(X, N) \endcode performs zero-padding up to size N and then
     performs an N-size fft.
 
-    The implementation is built upon one of the following libraries: FFTW,
-    MKL, or ACML, depending on which was chosen during compilation.
+    The implementation is built upon one of the following libraries: 
+    - FFTW (version 3.0.0 or higher)
+    - MKL (version 8.0.0 or higher) 
+    - ACML (version 2.5.3 or higher).
+    
+    \note FFTW-based implementation is the fastest for powers of two.
+    Furthermore, the second time you call the routine with the same size,
+    the calculation is much faster due to many things were calculated and
+    stored the first time the routine was called.
 
-
-    The routine is fastest for powers of two. Furthermore, the second time
-    you call the routine with the same size, the calculation is much faster
-    due to many things were calculated and stored the first time the routine
-    was called.
-
-    <b>Note to FFTW users:</b> Achieving maximum runtime efficiency
-    with the FFTW library on some computer architectures requires that
-    data are stored in the memory with a special alignment (to 16-byte
-    boundaries).  The IT++ memory management functions and container
-    classes do not generally allocate memory aligned this way, and as
-    a result calling FFTW via the IT++ interface (i.e. the fft()
-    function) may be slower than using the FFTW library directly.
-    Therefore, FFTW users concerned about maximum possible performance
-    may want to consider the possibility of calling the FFTW library
-    and its memory management/allocation routines directly, bypassing
-    the IT++ storage classes and the fft() interface to FFTW.
+    \note Achieving maximum runtime efficiency with the FFTW library on some
+    computer architectures requires that data are stored in the memory with
+    a special alignment (to 16-byte boundaries). The IT++ memory management
+    functions and container classes do not generally allocate memory aligned
+    this way, and as a result calling FFTW via the IT++ interface (i.e. the
+    fft() function) may be slower than using the FFTW library directly.
+    Therefore, FFTW users concerned about maximum possible performance may
+    want to consider the possibility of calling the FFTW library and its
+    memory management/allocation routines directly, bypassing the IT++
+    storage classes and the fft() interface to FFTW.
   */
 
   //!\addtogroup fft
@@ -132,14 +130,27 @@ namespace itpp {
     where \f$w(k) = 1/sqrt{N}\f$ for \f$k=0\f$ and 
     \f$w(k) = sqrt{2/N}\f$ for \f$k\geq 1\f$.
 
+    The implementation is built upon one of the following libraries: 
+    - FFTW (version 3.0.0 or higher)
+    - MKL (version 8.0.0 or higher) 
+    - ACML (version 2.5.3 or higher).
 
-    The implementation is built upon one of the following libraries: FFTW,
-    MKL, or ACML, depending on which was chosen during compilation.
+    \note FFTW-based implementation is the fastest for powers of two.
+    Furthermore, the second time you call the routine with the same size,
+    the calculation is much faster due to many things were calculated and
+    stored the first time the routine was called.
 
-    The routine is fastest for powers of two. Furthermore, the second time
-    you call the routine with the same size, the calculation is much faster
-    due to many things were calculated and stored the first time the routine
-    was called.
+    \note Achieving maximum runtime efficiency with the FFTW library on some
+    computer architectures requires that data are stored in the memory with
+    a special alignment (to 16-byte boundaries). The IT++ memory management
+    functions and container classes do not generally allocate memory aligned
+    this way, and as a result calling FFTW via the IT++ interface (i.e. the
+    dct()/idct() function) may be slower than using the FFTW library
+    directly. Therefore, FFTW users concerned about maximum possible
+    performance may want to consider the possibility of calling the FFTW
+    library and its memory management/allocation routines directly,
+    bypassing the IT++ storage classes and the dct()/idct() interface to
+    FFTW.
   */
 
   //!\addtogroup dct
@@ -179,9 +190,163 @@ namespace itpp {
   template <class T> Mat<T> dwht2(const Mat<T> &m);
   //!@}
 
-  /////////////////////////////
+  template <class T>
+  Vec<T> dht(const Vec<T> &v)
+  {
+    Vec<T> ret(v.size());
+    dht(v, ret);
+    return ret;
+  }
+
+  //! Bit reverse
+  template <class T>
+  void bitrv(Vec<T> &out) 
+  {
+    int N = out.size();
+    int j = 0;
+    int N1 = N-1;
+    for (int i = 0; i < N1; ++i) {
+      if (i < j) {
+	T temp = out[j];
+	out[j] = out[i];
+	out[i] = temp;
+      }
+      int K = N/2;
+      while (K <= j) {
+	j -= K;
+	K /= 2;
+      }
+      j += K;
+    }
+  }
+
+  template <class T>
+  void dht(const Vec<T> &vin, Vec<T> &vout)
+  {
+    int N = vin.size();
+    int m = levels2bits(N);
+    it_assert1((1<<m) == N, "dht(): The vector size must be a power of two");
+
+    vout.set_size(N);
+
+    // This step is separated because it copies vin to vout
+    for (int ib = 0; ib < N; ib += 2) {
+      vout(ib) = vin(ib) + vin(ib+1);
+      vout(ib+1) = vin(ib) - vin(ib+1);
+    }
+    N /= 2;
+
+    int l = 2;    
+    for (int i = 1; i < m; ++i) {
+      N /= 2;
+      int ib = 0;
+      for (int k = 0; k < N; ++k) {
+	for (int j = 0; j < l; ++j) {
+	  T t = vout(ib+j);
+	  vout(ib+j) += vout(ib+j+l);
+	  vout(ib+j+l) = t - vout(ib+j+l);
+	}
+	ib += 2*l;
+      }
+      l *= 2;
+    }
+
+    vout /= static_cast<T>(std::sqrt(static_cast<double>(vin.size())));
+  }
+
+  template <class T>
+  void self_dht(Vec<T> &v)
+  {
+    int N = v.size();
+    int m = levels2bits(N);
+    it_assert1((1<<m) == N, "self_dht(): The vector size must be a power "
+		    "of two");
+
+    int l = 1;
+    for (int i = 0; i < m; ++i) {
+      N /= 2;
+      int ib = 0;
+      for (int k = 0; k < N; ++k) {
+	for (int j = 0; j < l; ++j) {
+	  T t = v(ib+j);
+	  v(ib+j) += v(ib+j+l);
+	  v(ib+j+l) = t - v(ib+j+l);
+	}
+	ib += 2*l;
+      }
+      l *= 2;
+    }
+
+    v /= static_cast<T>(std::sqrt(static_cast<double>(v.size())));
+  }
+
+  template <class T>
+  Vec<T> dwht(const Vec<T> &v)
+  {
+    Vec<T> ret(v.size());
+    dwht(v, ret);
+    return ret;
+  }
+
+  template <class T>
+  void dwht(const Vec<T> &vin, Vec<T> &vout)
+  {
+    dht(vin, vout);
+    bitrv(vout);
+  }
+
+
+  template <class T>
+  void self_dwht(Vec<T> &v)
+  {
+    self_dht(v);
+    bitrv(v);
+  }
+
+  template <class T>
+  Mat<T> dht2(const Mat<T> &m)
+  {
+    Mat<T> ret(m.rows(), m.cols());
+    Vec<T> v;
+
+    for (int i = 0; i < m.rows(); ++i) {
+      v = m.get_row(i);
+      self_dht(v);
+      ret.set_row(i, v);
+    }
+    for (int i = 0; i < m.cols(); ++i) {
+      v = ret.get_col(i);
+      self_dht(v);
+      ret.set_col(i, v);
+    }
+
+    return transpose(ret);
+  }
+
+  template <class T>
+  Mat<T> dwht2(const Mat<T> &m)
+  {
+    Mat<T> ret(m.rows(), m.cols());
+    Vec<T> v;
+
+    for (int i = 0; i < m.rows(); ++i) {
+      v = m.get_row(i);
+      self_dwht(v);
+      ret.set_row(i, v);
+    }
+    for (int i = 0; i < m.cols(); ++i) {
+      v = ret.get_col(i);
+      self_dwht(v);
+      ret.set_col(i, v);
+    }
+
+    return transpose(ret);
+  }
+
+
+  // ----------------------------------------------------------------------
   // template instantiation
-  /////////////////////////////
+  // ----------------------------------------------------------------------
 #ifndef _MSC_VER
 
   //! Template instantiation of dht
