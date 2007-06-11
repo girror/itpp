bd3c5833f3ec02db7798a1051449bb1a09e79fa3
Adam Piątyszek <ediap@users.sourceforge.net>
2007-06-11 11:58:31 +0000

    Improvements in MKL detection macros
    The latest MKL release 9.1.018 has different naming of a lapack
    shared library (libmkl_lapack32.so and libmkl_lapack64.so ->
    libmkl_lapack.so). Cosmetic changes in MKL's FFT macro and variable
    names (MKL8 -> MKL).
    
    
    
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/branches/itpp-3-10@1060 a6533b66-300f-0410-b061-b36de6c4fb2d

--- itpp/config_msvc.h
+++ itpp/config_msvc.h
@@ -68,8 +68,8 @@
 #endif
 
 #ifdef HAVE_MKL
-/* Define if you have MKL8 FFT library. */
-#  define HAVE_FFT_MKL8 1
+/* Define if you have MKL FFT library. */
+#  define HAVE_FFT_MKL 1
 #endif
 
 /* Define to 1 if you have the <fstream> header file. */
